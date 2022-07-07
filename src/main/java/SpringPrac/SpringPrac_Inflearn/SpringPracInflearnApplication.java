a3f9a3ea43c875396e64e8cd595b508d652b620d
yoonajang <o119a@naver.com>
2022-07-07 14:12:22 +0900

    Initial commit

---
+++ src/main/java/SpringPrac/SpringPrac_Inflearn/SpringPracInflearnApplication.java
@@ -0,0 +1,13 @@
+package SpringPrac.SpringPrac_Inflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracInflearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracInflearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringPrac/SpringPrac_Inflearn/SpringPracInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringPrac.SpringPrac_Inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracInflearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
